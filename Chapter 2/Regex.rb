276b82dccc05dda658c99aae1ab34cdb1d08a5d7
oriphin <oriphinz@gmail.com>
2012-07-02 00:29:37 +1000

    Added regex

---
+++ Chapter 2/Regex.rb
@@ -0,0 +1,18 @@
+# Regex to match Perl and Python
+/Perl|Python/
+/P(erl|ython)/
+
+/\d\d:\d\d:\d\d/ 			# a time such as 12:34:56
+/Perl.*Python/ 				# Perl, zero or more other chars, then Python # Perl, a space, and Python
+/Perl Python/					# Perl, a space, and Python
+/Perl *Python/				# Perl, zero or more spaces, and Python
+/Perl +Python/				# Perl, one or more spaces, and Python
+/Perl\s+Python/				# Perl, whitespace characters, then Python
+/Ruby (Perl|Python)/	# Ruby, a space, and either Perl or Python
+
+if line =~ /Perl|Python/
+	puts "Scripting language mentioned: #{line}"
+end
+
+line.sub(/Perl/, 'Ruby') # replace first 'Perl' with 'Ruby' 
+line.gsub(/Python/, 'Ruby') # replace every 'Python' with 'Ruby'
